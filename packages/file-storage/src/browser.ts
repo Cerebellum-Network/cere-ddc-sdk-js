8bf59c4664d2ce0d47558f1f59bec44a8e8faa06
svalasovich <svalasovich@gmail.com>
2022-04-22 13:38:33 +0400

    clean up

--- packages/file-storage/src/browser.ts
+++ packages/file-storage/src/browser.ts
@@ -2,7 +2,7 @@ export {FileStorageConfig, KB, MB} from "./core/FileStorageConfig";
 
 import {PieceUri, SchemeInterface} from "@cere-ddc-sdk/content-addressable-storage";
 import {FileStorageConfig} from "./core/FileStorageConfig";
-import {CoreFileStorage} from "./core/FileStorage";
+import {CoreFileStorage} from "./core/CoreFileStorage";
 import {FileStorageInterface} from "./core/FileStorage.interface";
 
 export class FileStorage implements FileStorageInterface {
