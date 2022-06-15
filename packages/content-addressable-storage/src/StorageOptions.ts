91143a825e2d32589730941f0b9df24c776a73ce
svalasovich <svalasovich@gmail.com>
2022-06-15 11:01:21 +0300

    added discovery from smart-contract by cluster id

---
+++ packages/content-addressable-storage/src/StorageOptions.ts
@@ -0,0 +1,30 @@
+import {Options as SmartContractOptions, TESTNET} from "@cere-ddc-sdk/smart-contract";
+import {CidBuilder, CipherInterface, NaclCipher, SchemeInterface, SchemeType} from "@cere-ddc-sdk/core";
+
+export class StorageOptions {
+    clusterAddress: string | number; // Cluster ID or CDN URL
+    smartContract?: SmartContractOptions = TESTNET;
+    scheme?: SchemeType | SchemeInterface = "sr25519";
+    cipher?: CipherInterface = new NaclCipher();
+    cidBuilder?: CidBuilder = new CidBuilder();
+
+    constructor(clusterAddress: string | number = "") {
+        this.clusterAddress = clusterAddress;
+    }
+}
+
+const defaultOptions = new StorageOptions();
+
+export const initDefaultOptions = (options: StorageOptions): StorageOptions => {
+    if (!options.clusterAddress) {
+        throw new Error(`invalid clusterAddress='${options.clusterAddress}'`)
+    }
+
+    return {
+        clusterAddress: options.clusterAddress,
+        smartContract: options.smartContract || defaultOptions.smartContract,
+        scheme: options.scheme || defaultOptions.scheme,
+        cipher: options.cipher || defaultOptions.cipher,
+        cidBuilder: options.cidBuilder || defaultOptions.cidBuilder
+    }
+}
