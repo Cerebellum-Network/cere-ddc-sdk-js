def73a64cd7945aef6d86edff351f3a6500db37f
svalasovich <svalasovich@gmail.com>
2022-06-24 12:34:49 +0300

    fix export

--- packages/smart-contract/src/type.ts
+++ packages/smart-contract/src/type.ts
@@ -11,9 +11,9 @@ import {Signer as InjectedSigner} from "@polkadot/api/types";
 
 export {Options, DEVNET, TESTNET, MAINNET} from "./Options.js"
 export {BucketCreatedEvent} from "./event/BucketCreatedEvent.js"
-/*export {BucketPermissionGrantedEvent} from "./event/BucketPermissionGrantedEvent.js"
+export {BucketPermissionGrantedEvent} from "./event/BucketPermissionGrantedEvent.js"
 export {BucketPermissionRevokedEvent} from "./event/BucketPermissionRevokedEvent.js"
-export {Permission} from "./model/Permission.js"*/
+export {Permission} from "./model/Permission.js"
 export {BucketStatusList} from "./model/BucketStatusList.js"
 export {BucketStatus} from "./model/BucketStatus.js"
 
