d387df46f3c29c18d3ef027c29fa00e3ffe4d66d
svalasovich <svalasovich@gmail.com>
2022-06-28 12:22:19 +0300

    added searchType

--- packages/content-addressable-storage/src/index.ts
+++ packages/content-addressable-storage/src/index.ts
@@ -2,7 +2,7 @@ export {Piece} from "./models/Piece.js";
 export {PieceUri} from "./models/PieceUri.js";
 export {Query} from "./models/Query.js";
 export {SearchResult} from "./models/SearchResult.js";
-export {Tag} from "./models/Tag.js";
+export {Tag, SearchType} from "./models/Tag.js";
 export {Link} from "./models/Link.js";
 export {EncryptionOptions} from "./EncryptionOptions.js";
 export {ContentAddressableStorage, DEK_PATH_TAG} from "./ContentAddressableStorage.js";
--- packages/content-addressable-storage/src/models/Tag.ts
+++ packages/content-addressable-storage/src/models/Tag.ts
@@ -1,9 +1,16 @@
 export class Tag {
   key: string;
   value: string;
+  searchable: SearchType;
 
-  constructor(key: string, value: string) {
+  constructor(key: string, value: string, searchable: SearchType = SearchType.RANGE) {
     this.key = key;
     this.value = value;
+    this.searchable = searchable;
   }
+}
+
+export enum SearchType {
+  RANGE             = 0,
+  NOT_SEARCHABLE    = 1
 }
