11b8b945d0a6607b28c6c73a223ba4fe2bea724b
shamilkhan <shamilkhan1999@gmail.com>
2022-12-14 18:42:58 +0400

    feat(tiers): add public export for tiers

--- packages/content-addressable-storage/src/index.ts
+++ packages/content-addressable-storage/src/index.ts
@@ -4,6 +4,7 @@ export {Query} from "./models/Query";
 export {SearchResult} from "./models/SearchResult";
 export {Tag, SearchType} from "./models/Tag";
 export {Link} from "./models/Link";
+export {Tier} from "./models/Tier";
 export {EncryptionOptions} from "./EncryptionOptions";
 export {ContentAddressableStorage} from "./ContentAddressableStorage";
 export {DEK_PATH_TAG} from "./constants";
