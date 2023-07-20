49bd8dbac6756d5cebbdfbfa8263c643a64195da
Sergey Kambalin <sergey.kambalin@cere.io>
2023-07-20 12:01:27 +0600

    Allow passing extension origin name

--- packages/smart-contract/src/browser.ts
+++ packages/smart-contract/src/browser.ts
@@ -1,11 +1,14 @@
 import {SmartContract as CoreSmartContract} from './SmartContract';
-import {SmartContractOptions} from './options/SmartContractOptions';
+import {SmartContractOptions as BaseSmartContractOptions} from './options/SmartContractOptions';
 import {InjectedAccount} from '@polkadot/extension-inject/types';
 import {web3Enable, web3FromAddress} from '@polkadot/extension-dapp';
 import {Signer} from '@polkadot/api/types';
 
 export {DEVNET, TESTNET, MAINNET} from './options/SmartContractOptions';
-export type {SmartContractOptions} from './options/SmartContractOptions';
+
+export type SmartContractOptions = BaseSmartContractOptions & {
+    extensionOriginName?: string;
+};
 
 export class SmartContract extends CoreSmartContract {
     static async buildAndConnect(
@@ -15,7 +18,7 @@ export class SmartContract extends CoreSmartContract {
         if (typeof accountOrSecretPhrase === 'string') {
             return super.buildAndConnect(accountOrSecretPhrase, options);
         } else {
-            await web3Enable('ddc');
+            await web3Enable(options?.extensionOriginName || 'ddc');
             const {address} = accountOrSecretPhrase;
             const {signer} = await web3FromAddress(address);
 
