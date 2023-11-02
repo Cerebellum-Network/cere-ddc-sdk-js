ece22eff07ad04d7ba3e5d756f1b639323498d14
Sergey Kambalin <sergey.kambalin@cere.io>
2023-11-02 18:17:28 +0600

    Fixes for e2e tests integration

--- packages/ddc-client/src/DdcUri.ts
+++ packages/ddc-client/src/DdcUri.ts
@@ -1,8 +1,17 @@
 export type DdcEntity = 'file' | 'dag-node';
 
-export type FileUri = DdcUri<'file'>;
-export type DagNodeUri = DdcUri<'dag-node'>;
-
 export class DdcUri<T extends DdcEntity = DdcEntity> {
     constructor(readonly bucketId: number, readonly cid: string, readonly entity: T) {}
+}
+
+export class FileUri extends DdcUri<'file'> {
+    constructor(bucketId: number, cid: string) {
+        super(bucketId, cid, 'file');
+    }
+}
+
+export class DagNodeUri extends DdcUri<'dag-node'> {
+    constructor(bucketId: number, cid: string) {
+        super(bucketId, cid, 'dag-node');
+    }
 }
