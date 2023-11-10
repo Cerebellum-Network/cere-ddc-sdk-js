ec7601330de550d110e238a9fa8c333d935a0c1f
Sergey Kambalin <sergey.kambalin@cere.io>
2023-11-10 17:46:07 +0600

    Remove browser polyfills from globalSetup

--- tests/setup/globalSetup.cjs
+++ tests/setup/globalSetup.cjs
@@ -5,11 +5,5 @@ const {startEnvironment} = require('./environment');
 module.exports = async () => {
     console.log(''); // New line
 
-    /**
-     * Emulate browser environment
-     */
-    globalThis.self = globalThis;
-    globalThis.WebSocket = require('ws').WebSocket;
-
     await startEnvironment();
 };
