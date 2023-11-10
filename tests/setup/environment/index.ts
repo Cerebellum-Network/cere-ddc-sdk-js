1161daed4432e533b93d1bbb87b67ef4595d2777
Ivan Dmitriev <100341+idmitriev@users.noreply.github.com>
2023-11-10 18:51:18 +0400

    Remove unused code

--- tests/setup/environment/index.ts
+++ tests/setup/environment/index.ts
@@ -1,11 +1,6 @@
 import {startDDC, stopDDC} from './ddc';
 import {startBlockchain, stopBlockchain} from './blockchain';
 
-declare global {
-    var DDC_CONTRACT_ADDRESS: string;
-    var DDC_API_URL: string;
-}
-
 export const startEnvironment = async () => {
     console.group('Start local environment');
 
