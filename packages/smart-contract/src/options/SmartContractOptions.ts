48daea3f1cbde232c1c1a77873b368b4d1097b51
antonmazhuto <anton.mazhuto@cere.io>
2023-11-14 20:06:54 +0300

    Update testnet

--- packages/smart-contract/src/options/SmartContractOptions.ts
+++ packages/smart-contract/src/options/SmartContractOptions.ts
@@ -9,20 +9,20 @@ export type SmartContractOptions = {
 };
 
 export const MAINNET: SmartContractOptions = {
-    rpcUrl: 'wss://rpc.qanet.cere.network/ws',
-    contractAddress: '6Q7C6G3Hpz43tSDPotqT1wYHupTpEJ54oAv3QrAM7TnCF2oY',
+    rpcUrl: 'wss://rpc.testnet.cere.network/ws',
+    contractAddress: '6V8CVz1iuDH361tizGzcrdW8xxF775Rjmv6Hg2gz6fqfmEsg',
     abi: ddcBucketAbi,
 };
 
 export const TESTNET: SmartContractOptions = {
     rpcUrl: 'wss://rpc.testnet.cere.network/ws',
-    contractAddress: '6TokurXU4KdNUgHm85tPrFad1cVVT2QZp7jnCxdciFLfF1Mj',
+    contractAddress: '6V8CVz1iuDH361tizGzcrdW8xxF775Rjmv6Hg2gz6fqfmEsg',
     abi: ddcBucketAbi,
 };
 
 export const DEVNET: SmartContractOptions = {
-    rpcUrl: 'wss://archive.devnet.cere.network/ws',
-    contractAddress: '6U7MUyRTQNLVwTao9JuvDFoiJxsk6HNtQZ8XFswPrkLmQPjx',
+    rpcUrl: 'wss://rpc.testnet.cere.network/ws',
+    contractAddress: '6TaAuTbpfuCUMcQyyaRyzeoEsMYehJmaPLPeXhEcHkwr9J6v',
     abi: ddcBucketAbi,
 };
 
