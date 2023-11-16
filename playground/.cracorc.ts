ca2fcb41e5827a52e2619c5f1433cd5f7acc08f0
Sergey Kambalin <sergey.kambalin@cere.io>
2023-11-16 17:45:33 +0600

    Fix playground build

--- playground/.cracorc.ts
+++ playground/.cracorc.ts
@@ -1,17 +1,23 @@
-import {DefinePlugin} from 'webpack';
+import {Configuration, DefinePlugin} from 'webpack';
 import NodePolyfill from 'node-polyfill-webpack-plugin';
+import {addPlugins} from '@craco/craco';
 
 import {BLOCKCHAIN_RPC_URL} from '../tests';
 
 export default {
     webpack: {
-        plugins: {
-            add: [
+        configure: (config: Configuration) => {
+            config.resolve ||= {};
+            config.resolve.plugins = [];
+
+            addPlugins(config, [
                 new NodePolyfill(),
                 new DefinePlugin({
-                    'process.env.BC_ENDPOINT': BLOCKCHAIN_RPC_URL,
+                    'process.env.BC_ENDPOINT': JSON.stringify(BLOCKCHAIN_RPC_URL),
                 }),
-            ],
+            ]);
+
+            return config;
         },
     },
 };
