169c97d75e1a62536dbe9890f0063f780eff0008
Sergey Kambalin <sergey.kambalin@cere.io>
2023-11-23 10:21:33 +0600

    Switch back to sr25519 by default

--- packages/blockchain/src/Signer/UriSigner.ts
+++ packages/blockchain/src/Signer/UriSigner.ts
@@ -41,11 +41,7 @@ export class UriSigner implements Signer {
   }
 
   get type() {
-    /**
-     * TODO: Discuss with the team since switching from `sr25519` to `ed25519` by defualt can lead to problems with pre-funded dev accounts.
-     * It might be better to add `sr25519` support to storage node ACKs signatures
-     */
-    return this.options.type || 'ed25519';
+    return this.options.type || 'sr25519';
   }
 
   get address() {
