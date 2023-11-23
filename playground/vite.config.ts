354da5c417469c3defd4817eb83556cf48c89155
Sergey Kambalin <sergey.kambalin@cere.io>
2023-11-23 09:58:29 +0600

    Playground base url config

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -6,6 +6,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
  * https://vitejs.dev/config/
  */
 export default defineConfig({
+  base: './',
   plugins: [react(), nodePolyfills()],
   define: {
     'process.env.BC_ENDPOINT': JSON.stringify('ws://localhost:9944'),
