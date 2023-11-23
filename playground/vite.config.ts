d1404efee128e9908944adacdb07a8af771d185e
Sergey Kambalin <kambalin@gmail.com>
2023-11-23 11:57:36 +0600

    Typescript upgrade to v5 (#166)
    * Test env bootstrap script
    
    * Playground to Vite
    
    * Playground base url config
    
    * Upgrade typescript
    
    * Remove unused tests main field
    
    * Fix playground deployment

---
+++ playground/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
+/**
+ * https://vitejs.dev/config/
+ */
+export default defineConfig({
+  base: './',
+  plugins: [react(), nodePolyfills()],
+  define: {
+    'process.env.BC_ENDPOINT': JSON.stringify('ws://localhost:9944'),
+  },
+});
