44bb3d7eff93963a38f5fbe37bc795477b40942d
Sergey Kambalin <sergey.kambalin@cere.io>
2023-11-24 12:06:41 +0600

    Always pre-build packages before tests

--- tests/jest.config.ts
+++ tests/jest.config.ts
@@ -1,21 +1,12 @@
-import * as path from 'path';
+import 'ts-jest';
 import type { Config } from 'jest';
-import { pathsToModuleNameMapper } from 'ts-jest';
-import * as paths from 'tsconfig-paths';
 
-import { compilerOptions } from '../tsconfig.json';
-
-const projectRoot = path.resolve(__dirname, '..');
-
-paths.register({
-  baseUrl: compilerOptions.baseUrl,
-  paths: compilerOptions.paths,
-});
-
-const common: Config = {
+const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>'],
+  verbose: true,
+  testTimeout: 100000,
 
   testMatch: ['<rootDir>/specs/**/*.spec.ts'],
   globalTeardown: './setup/globalTeardown.ts',
@@ -23,30 +14,13 @@ const common: Config = {
   setupFilesAfterEnv: ['./setup/setup.ts'],
 
   transform: {
-    '\\.(js|ts)$': ['ts-jest', { useESM: true, tsconfig: path.resolve(projectRoot, 'tsconfig.json') }],
-  },
-};
-
-const config: Config = {
-  verbose: true,
-  testTimeout: 100000,
-  projects: [
-    {
-      ...common,
-      displayName: 'CI',
-      transform: {
-        '\\.(js|ts)$': ['ts-jest', { tsconfig: path.resolve(projectRoot, 'tsconfig.build.json') }],
+    '\\.(js|ts)$': [
+      'ts-jest',
+      {
+        tsconfig: '../tsconfig.build.json',
       },
-    },
-    {
-      ...common,
-      displayName: 'Dev',
-      modulePaths: [compilerOptions.baseUrl],
-      moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-        prefix: projectRoot,
-      }),
-    },
-  ],
+    ],
+  },
 };
 
 export default config;
