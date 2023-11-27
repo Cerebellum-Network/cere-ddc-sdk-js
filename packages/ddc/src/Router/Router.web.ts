df1de1596e9e4547a9bb2b207a53dfdbcf1078da
Sergey Kambalin <sergey.kambalin@cere.io>
2023-11-27 18:51:45 +0600

    Detect if ssl is required

--- packages/ddc/src/Router/Router.web.ts
+++ packages/ddc/src/Router/Router.web.ts
@@ -1,8 +1,10 @@
 import { BaseRouter, RouterOperation, RouterNode } from './BaseRouter';
 
+const isSSLRequired = () => globalThis.location?.protocol === 'https:';
+
 export class Router extends BaseRouter {
   selectNode(operation: RouterOperation, nodes: RouterNode[]): RouterNode {
-    const sslNodes = nodes.filter((node) => node.ssl);
+    const sslNodes = isSSLRequired() ? nodes.filter((node) => node.ssl) : nodes;
     const randomIndex = Math.floor(Math.random() * sslNodes.length);
 
     return sslNodes[randomIndex];
