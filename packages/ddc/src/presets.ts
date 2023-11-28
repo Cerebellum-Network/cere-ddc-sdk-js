729c57acd7317b00060b00ec17a06fa300f95cc3
Sergey Kambalin <sergey.kambalin@cere.io>
2023-11-28 10:44:27 +0600

    Update DEVNET preset

--- packages/ddc/src/presets.ts
+++ packages/ddc/src/presets.ts
@@ -13,13 +13,12 @@ export const DEVNET: ConfigPreset = {
   blockchain: 'wss://archive.devnet.cere.network/ws',
   nodes: [
     { grpcUrl: 'grpc://38.242.236.247:9090', httpUrl: 'https://storage-1.ddc-devnet.cloud', ssl: true },
-    { grpcUrl: 'grpc://38.242.236.247:9090', httpUrl: 'http://38.242.236.247:8080' },
     { grpcUrl: 'grpc://38.242.236.247:9091', httpUrl: 'http://38.242.236.247:8081' },
-    { grpcUrl: 'grpc://158.220.87.61:9090', httpUrl: 'http://158.220.87.61:8080' },
+    { grpcUrl: 'grpc://158.220.87.61:9090', httpUrl: 'https://storage-3.ddc-devnet.cloud', ssl: true },
     { grpcUrl: 'grpc://158.220.87.61:9091', httpUrl: 'http://158.220.87.61:8081' },
-    { grpcUrl: 'grpc://89.117.79.111:9090', httpUrl: 'http://89.117.79.111:8080' },
+    { grpcUrl: 'grpc://89.117.79.111:9090', httpUrl: 'https://storage-5.ddc-devnet.cloud', ssl: true },
     { grpcUrl: 'grpc://89.117.79.111:9091', httpUrl: 'http://89.117.79.111:8081' },
-    { grpcUrl: 'grpc://154.53.57.124:9090', httpUrl: 'http://154.53.57.124:8080' },
+    { grpcUrl: 'grpc://154.53.57.124:9090', httpUrl: 'https:///storage-7.ddc-devnet.cloud', ssl: true },
     { grpcUrl: 'grpc://154.53.57.124:9091', httpUrl: 'http://154.53.57.124:8081' },
   ],
 };
