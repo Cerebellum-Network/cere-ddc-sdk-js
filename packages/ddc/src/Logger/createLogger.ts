ead3c433388ed5355497cbf92a8ddfcc4488fbf5
Sergey Kambalin <sergey.kambalin@cere.io>
2023-12-01 13:24:17 +0600

    Change default log level to warn

--- packages/ddc/src/Logger/createLogger.ts
+++ packages/ddc/src/Logger/createLogger.ts
@@ -3,7 +3,7 @@ import { Logger, LoggerOptions } from './types';
 
 export const createLogger = (options: LoggerOptions = {}): Logger =>
   pino({
-    level: options.logLevel || 'info',
+    level: options.logLevel || 'warn',
     msgPrefix: options.prefix ? `[${options.prefix}] ` : undefined,
     transport: {
       target: 'pino-pretty',
