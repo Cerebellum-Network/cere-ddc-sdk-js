9c4844af9f4fbad8def6b170641f2fbdcd7642b4
Sergey Kambalin <sergey.kambalin@cere.io>
2024-01-08 12:45:51 +0600

    Update test scenarios to not repeat the same steps

--- tests/specs/DdcApis.spec.ts
+++ tests/specs/DdcApis.spec.ts
@@ -31,8 +31,7 @@ const wholeSpecVariants = [
 const fileSpecVariants = [
   { name: 'with ACKs', enableAcks: true, authenticate: false },
   { name: 'without ACKs', enableAcks: false, authenticate: false },
-  { name: 'with proofs', enableAcks: true, authenticate: true },
-  { name: 'without proofs', enableAcks: true, authenticate: false },
+  { name: 'ACKs + proofs', enableAcks: true, authenticate: true },
 ];
 
 describe.each(wholeSpecVariants)('DDC APIs ($name)', ({ transport }) => {
