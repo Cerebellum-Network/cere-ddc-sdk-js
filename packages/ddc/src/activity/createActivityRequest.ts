7321a0d106487f1c94e75de77dadcdd991038b8c
Sergey Kambalin <kambalin@gmail.com>
2024-02-20 21:32:09 +0600

    feat(ddc): Add activity request header to CNS and DAG API (#232)

---
+++ packages/ddc/src/activity/createActivityRequest.ts
@@ -0,0 +1,46 @@
+import { v4 as uuid } from 'uuid';
+import { Buffer } from 'buffer';
+import type { Signer } from '@cere-ddc-sdk/blockchain';
+
+import type { Logger } from '../logger';
+import {
+  ActivityRequest,
+  ActivityRequest_ContentType,
+  ActivityRequest_RequestType,
+} from '../grpc/activity_report/activity_report';
+import { createSignature } from '../signature';
+
+export { ActivityRequest_RequestType as ActivityRequestType };
+export type CreateActivityRequestOptions = {
+  signer?: Signer;
+  logger?: Logger;
+};
+
+/**
+ * Creates an activity request.
+ */
+export const createActivityRequest = async (
+  request: Partial<Omit<ActivityRequest, 'contentType' | 'signature'>>,
+  { signer, logger }: CreateActivityRequestOptions,
+) => {
+  if (!signer) {
+    throw new Error('Activity capturing cannot be enabled. Signer requred!');
+  }
+
+  const activityRequest = ActivityRequest.create({
+    timestamp: Date.now(),
+    requestId: createRequestId(),
+    ...request,
+    contentType: ActivityRequest_ContentType.PIECE,
+  });
+
+  activityRequest.signature = await createSignature(signer, ActivityRequest.toBinary(activityRequest));
+  logger?.debug({ activityRequest }, 'Activity request');
+
+  return Buffer.from(ActivityRequest.toBinary(activityRequest)).toString('base64');
+};
+
+/**
+ * Generates a unique request ID.
+ */
+export const createRequestId = () => uuid();
