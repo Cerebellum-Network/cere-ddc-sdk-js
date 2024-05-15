80325f2e828469fbc67dfb0c76b86fae56c50f51
Sergey Kambalin <sergey.kambalin@cere.io>
2024-05-15 13:34:28 +0600

    feat: Add JSON signers support to CLI

--- packages/cli/src/account.ts
+++ packages/cli/src/account.ts
@@ -1,23 +1,21 @@
-import { DdcClient, UriSigner } from '@cere-ddc-sdk/ddc-client';
+import { DdcClient } from '@cere-ddc-sdk/ddc-client';
 
-import { CreateClientOptions } from './createClient';
+import { CreateClientOptions, createSigner } from './createClient';
 import { CERE } from './constants';
 
 export type AccountOptions = Pick<CreateClientOptions, 'signer' | 'signerType'>;
 
 export const account = async (client: DdcClient | null, { signer, signerType }: AccountOptions) => {
-  const uriSigner = new UriSigner(signer, {
-    type: signerType === 'ed25519' ? 'ed25519' : 'sr25519',
-  });
-
-  await uriSigner.isReady();
+  const realSigner = await createSigner(signer, signerType);
+  await realSigner.isReady();
 
   const [balance, deposit] = await Promise.all([client?.getBalance() || BigInt(0), client?.getDeposit() || BigInt(0)]);
 
   return {
     balance: Number(balance / BigInt(CERE)),
     deposit: Number(deposit / BigInt(CERE)),
-    address: uriSigner.address,
-    publicKey: '0x' + Buffer.from(uriSigner.publicKey).toString('hex'),
+    address: realSigner.address,
+    type: realSigner.type,
+    publicKey: '0x' + Buffer.from(realSigner.publicKey).toString('hex'),
   };
 };
