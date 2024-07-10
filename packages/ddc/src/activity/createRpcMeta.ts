5a360fc034e3fe4578ab17661eef55f2b239aae0
Józef Wołoch <jozef.woloch@cere.io>
2024-07-10 10:54:42 +0200

    Fix correlationId gRPC metadata key

--- packages/ddc/src/activity/createRpcMeta.ts
+++ packages/ddc/src/activity/createRpcMeta.ts
@@ -1,8 +1,10 @@
 import { v4 as uuid } from 'uuid';
 import { RpcMetadata } from '@protobuf-ts/runtime-rpc';
 
+const correlationIdMetadataKey = 'correlation-id'
+
 export type CorrelationMetaParams = { correlationId?: string };
 
 export const createRpcMeta = (correlationId?: string, meta?: RpcMetadata): RpcMetadata => {
-  return { ...meta, CorrelationID: correlationId || uuid() };
+  return { ...meta, [correlationIdMetadataKey]: correlationId || uuid() };
 };
