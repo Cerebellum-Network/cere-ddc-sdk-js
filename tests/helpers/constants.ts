03fd451cb80624f43b7feaf4e38e7f32e54ba019
Sergey Kambalin <kambalin@gmail.com>
2024-09-12 12:37:50 +0600

    chore(tests): Increase nodes startup time threshold in tests (#266)

--- tests/helpers/constants.ts
+++ tests/helpers/constants.ts
@@ -5,3 +5,5 @@ export const DDC_BLOCK_SIZE = 16 * 1024;
 export const ROOT_USER_SEED = 'hybrid label reunion only dawn maze asset draft cousin height flock nation';
 export const ROOT_ACCOUNT_TYPE = 'sr25519';
 export const BLOCKCHAIN_MAX_BLOCK_WEIGHT = 2_000_000_000_000;
+export const STORAGE_NODE_MAX_STARTUP_TIME = 60_000;
+export const BLOCKCHAIN_NODE_MAX_STARTUP_TIME = 30_000;
